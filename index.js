074740ec206d4f82f71eddda681260826563effb
benjamin2orie <ogechukwubenj@gmail.com>
2025-02-23 19:09:49 +0100

    fix cors conflict

--- index.js
+++ index.js
@@ -16,11 +16,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: 'http://example.com',
-  methods: ['GET', 'POST'],     
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
 
 
 
